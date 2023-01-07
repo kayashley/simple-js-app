7173495b2c207aceb2a2db67b349ceb5a57213f9
kayashley <itskaychay@gmail.com>
2023-01-06 22:10:18 -0500

    If applied, added a for loop to script js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
+//List of pokemon
 let pokemonList = [
-    {name: "Charmander", height: 6, types: ["fire, fly"]},
-    {name: "Pikachu", height: 5, types: ["electric, normal"]},
-    {name: "Squirtle", height: 5, types: ["water, rock"]}
-];
+    {name: "Charmander", height: 6, types: ["fire", "fly"]},
+    {name: "Pikachu", height: 5, types: ["electric", "normal"]},
+    {name: "Squirtle", height: 5, types: ["water", "rock"]}
+  ];
+
+  //Loop of pokemon
+  for (let i=0; i < pokemonList.length; i++) {
+    //If the height is less than 7 and greater than 5 it is big
+    if (pokemonList[i].height <7 && pokemonList[i].height >5) {
+      document.write(pokemonList[i].name + " - height: " + pokemonList[i].height + " I'm a big one! ");
+    //If the pokemon is less than 5 then it is average
+    }else {
+      document.write(pokemonList[i].name + " - height: " + pokemonList[i].height + " I'm average. ");
+    }
+  }
