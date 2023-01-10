c76ea137499ed8c29f65e32dbc6405edf7193546
kayashley <itskaychay@gmail.com>
2023-01-10 17:24:36 -0500

    If applied, forEach loop was added for the pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 //List of pokemon
 let pokemonList = [
-    {name: "Charmander", height: 6, types: ["fire", "fly"]},
-    {name: "Pikachu", height: 5, types: ["electric", "normal"]},
-    {name: "Squirtle", height: 5, types: ["water", "rock"]}
+    {name: "Charmander", height: 6, types: ["fire"]},
+    {name: "Pikachu", height: 5, types: ["electric"]},
+    {name: "Squirtle", height: 5, types: ["water"]}
   ];
 
+//forEach() loop
+pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + ' inches tall and is a ' + pokemon.type + ' type.')
+  });
+
   //Loop of pokemon
-  for (let i=0; i < pokemonList.length; i++) {
-    //If the height is less than 7 and greater than 5 it is big
-    if (pokemonList[i].height <7 && pokemonList[i].height >5) {
-      document.write(pokemonList[i].name + " - height: " + pokemonList[i].height + " I'm a big one! ");
-    //If the pokemon is less than 5 then it is average
-    }else {
-      document.write(pokemonList[i].name + " - height: " + pokemonList[i].height + " I'm average. ");
-    }
-  }
+//  for (let i=0; i < pokemonList.length; i++) {
+//    //If the height is less than 7 and greater than 5 it is big
+//    if (pokemonList[i].height <7 && pokemonList[i].height >5) {
+//      document.write(pokemonList[i].name + " - height: " + pokemonList[i].height + " I'm a big one! ");
+//   //If the pokemon is less than 5 then it is average
+//    }else {
+//      document.write(pokemonList[i].name + " - height: " + pokemonList[i].height + " I'm average. ");
+//    }
+//  }
