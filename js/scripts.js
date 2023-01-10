63d3f94cfa26fab465500c30f868c8177faa15bc
kayashley <itskaychay@gmail.com>
2023-01-10 18:00:25 -0500

    If applied, list of pokemon in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,50 @@
-//List of pokemon
-let pokemonList = [
-    {name: "Charmander", height: 6, type: ["fire"]},
-    {name: "Pikachu", height: 5, type: ["electric"]},
-    {name: "Squirtle", height: 5, type: ["water"]}
-  ];
+//IIFE 
+
+let pokemonRepository = (function () {
+    let pokemonList = [];
+
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+pokemonRepository.add( {name: "Charmander", height: 6, type: ["fire"]} );
+pokemonRepository.add( {name: "Pikachu", height: 5, type: ["electric"]});
+pokemonRepository.add( {name: "Squirtle", height: 5, type: ["water"]} );
+
+let pokemonList = pokemonRepository.getAll();
 
 //forEach() loop
 pokemonList.forEach(function(pokemon) {
-    console.log(pokemon.name + ' is ' + pokemon.height + ' inches tall and is a ' + pokemon.type + ' type.')
+        console.log(pokemon.name + ' is ' + pokemon.height + ' inches tall and is a ' + pokemon.type + ' type.')
   });
 
-  //Loop of pokemon
+
+
+
+
+
+//OLD List of pokemon
+//let pokemonList = [
+//    {name: "Charmander", height: 6, type: ["fire"]},
+//    {name: "Pikachu", height: 5, type: ["electric"]},
+//    {name: "Squirtle", height: 5, type: ["water"]}
+//  ];
+
+//forEach() loop
+//pokemonList.forEach(function(pokemon) {
+//    console.log(pokemon.name + ' is ' + pokemon.height + ' inches tall and is a ' + pokemon.type + ' type.')
+//  });
+
+//OLD Loop of pokemon
 //  for (let i=0; i < pokemonList.length; i++) {
 //    //If the height is less than 7 and greater than 5 it is big
 //    if (pokemonList[i].height <7 && pokemonList[i].height >5) {
